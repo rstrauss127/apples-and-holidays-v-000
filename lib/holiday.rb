018c9cc4dd49d2d9dcc7275e0352ba6b63437f93
Rebecca Strauss <rstrauss127@gmail.com>
2018-09-25 23:04:51 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -42,7 +42,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holidays|
     puts season.to_s.capitalize + ":"
     holidays.each do |hol, supplies|
-      puts hol.to_s.capitalize + ":"
+      puts hol.to_s.capitalize.chomp + ":"
       puts supplies.to_s.chomp
     end
   end
