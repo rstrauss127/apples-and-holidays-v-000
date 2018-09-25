66552be4e9966679b8f810efcc9550c667395ed5
Rebecca Strauss <rstrauss127@gmail.com>
2018-09-25 23:15:13 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -42,9 +42,9 @@ def all_supplies_in_holidays(holiday_hash)
      holiday.each do |holiday, item|
        puts "  #{holiday.to_s.split("_").map {|i| i.capitalize}.join(" ")}: #{item.join(", ")}"
      end
-  end 
+  end
 end
-end
+
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
