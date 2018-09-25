cd344677753a9721388755636080490852b32003
Rebecca Strauss <rstrauss127@gmail.com>
2018-09-25 22:57:17 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -41,8 +41,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holidays|
     puts season.to_s.capitalize + ":"
     holidays.each do |hol, supplies|
-      supplies.flatten
-      puts hol.to_s.capitalize + ":" + supplies.to_s
+      puts hol.to_s.capitalize + ":" + supplies.to_s.chomp
     end
   end
 end
